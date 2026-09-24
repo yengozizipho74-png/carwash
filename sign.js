05f04e49a90391dfd287bcc8f6c43ccde0f8a499
yengozizipho74-png <yengozizipho74@gmail.com>
2026-09-24 15:57:49 +0200

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,34 +1,31 @@
-function loginUser(event){
 
-    event.preventDefault();
-
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    if(email===""){
-        alert("Please eenter your email address.");
-        return;
-    }
-    if(password===""){
-        alert("Please enter your password.");
-        return;
-    }
-
-
-    // ===============================
-    // LOGIN CHECK
-    // ===============================
-
-    if (email.includes("admin")) {
-
-        window.location.href = "admin-dashboard.html";
-
-    } else {
-
-        window.location.href = "booking page.html";
-
-    }
-
-}
 
     
 
+function loginUser(event){
+  event.preventDefault();
+
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  if(email===""){
+    alert("Please enter your email address.");
+    return;
+  }
+  if(password===""){
+    alert("Please enter your password.");
+    return;
+  }
+
+  
+  let nameToShow = email.split("@")[0];
+  localStorage.setItem("userName", nameToShow);
+  localStorage.setItem("email", email);
+
+  // LOGIN CHECK
+  if (email.includes("admin")) {
+      window.location.href = "admin-dashboard.html";
+  } else {
+      window.location.href = "dashboard.html";
+  }
+}
